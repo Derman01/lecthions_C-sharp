054a6a40b20da37ab7fa24f09a7b72ba6b7b4317
Derman01 <54042384+Derman01@users.noreply.github.com>
2020-06-05 02:31:23 +0500

    добавлена диаграмма

--- Tables/Form1.cs
+++ Tables/Form1.cs
@@ -1,5 +1,6 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
 
@@ -8,6 +9,8 @@ namespace Tables
     public partial class Form1 : Form
     {
         public string FilePath;
+        Diagram f = new Diagram();
+
         public Form1()
         {
             InitializeComponent();
@@ -15,6 +18,7 @@ namespace Tables
             dataGridView1.Rows.Add("Евро", 0.9);
             dataGridView1.Rows.Add("Финская марка", 5.14);
             dataGridView1.Rows.Add("Английский фунт стерлингов", 0.62);
+            f.Show(); f.Visible = false;
         }
 
         private void calculat_Click(object sender, EventArgs e)
@@ -23,7 +27,7 @@ namespace Tables
             {
                 var rub = double.Parse(textBox1.Text) / 70;
                 double d;
-                for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
                     try
                     {
                         dataGridView1[2, i].Value = Math.Round( rub * Convert.ToDouble(dataGridView1.Rows[i].Cells[1].Value), 4);
@@ -46,7 +50,7 @@ namespace Tables
 
             var dlina = 40;
             var Name = new List<(string n, string c, string s)>();
-            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
                 Name.Add((dataGridView1[0, i].Value.ToString(),
                     dataGridView1[1, i].Value.ToString(),
                     dataGridView1[2, i].Value.ToString()));
@@ -76,5 +80,58 @@ namespace Tables
         {
             MessageBox.Show("Топычканов Денис Сергеевич\nИиП ПИ-19.02-1");
         }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            var f_add = new Form2();
+            f_add.ShowDialog();
+            if (f_add._add)
+            {
+                dataGridView1.Rows.Add(f_add.Value_name.Text, f_add.Course_name.Text);
+            }
+            f_add.Close();
+            update();
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            int _index = -1;
+            for (int i = 0; i < dataGridView1.SelectedRows.Count; i++)
+            {
+                _index = dataGridView1.SelectedRows[i].Index;
+            }
+            if (_index != - 1)
+                dataGridView1.Rows.Remove(dataGridView1.Rows[_index]);
+            update();
+        }
+
+        private void Open_Diagram_Click(object sender, EventArgs e)
+        {
+            
+            Open_Diagram.Checked = !Open_Diagram.Checked;
+            if (Open_Diagram.Checked)
+            {
+                f.Visible = true;
+                f.Location = new Point(Location.X + Width - 7, Location.Y + 30);
+                update();
+            }
+            else
+            {
+                f.Visible = false;
+            }
+        }
+        void update()
+        {
+            f.chart1.Series[0].Points.Clear();
+            for (var i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                f.chart1.Series[0].Points.Add(Convert.ToDouble(dataGridView1[1, i].Value));
+                f.chart1.Series[0].Points[i].LegendText = Convert.ToString(dataGridView1[0, i].Value);
+            }
+        }
+
+        private void Form1_LocationChanged(object sender, EventArgs e) => f.Location = new Point(Location.X + Width - 7, Location.Y + 33); 
+        private void Form1_SizeChanged(object sender, EventArgs e) => f.Location = new Point(Location.X + Width - 7, Location.Y + 33);
+        
     }
 }
