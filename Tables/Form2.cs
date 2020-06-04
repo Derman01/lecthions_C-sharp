a6ba6d7d5841cd2f48cecc3677c3865ae94e5214
Derman01 <54042384+Derman01@users.noreply.github.com>
2020-06-05 02:32:56 +0500

    код для формы добавления данных

---
+++ Tables/Form2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tables
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        public bool _add = false;
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            double d;
+            if (Double.TryParse(Course_name.Text, out d))
+            {
+                _add = true;
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Неправильно введен курс");
+            }
+           
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
