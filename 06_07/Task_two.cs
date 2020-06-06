b2f7554748ddcd7da8e56ebee7939f61895e4216
Derman01 <54042384+Derman01@users.noreply.github.com>
2020-06-06 18:08:39 +0500

    Add files via upload

---
+++ 06_07/Task_two.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Forms;
+
+namespace _06_07
+{
+    public partial class Task_two : Form
+    {
+        public Task_two()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add("Message");
+            comboBox1.Items.Add("About");
+        } 
+        private void comboBox1_SelectionChangeCommitted(object sender, System.EventArgs e)
+        {
+            switch (comboBox1.SelectedItem)
+            {
+                case "Message":
+                    MessageBox.Show("Окно-сообщение", "Задание 1");
+                    break;
+                case "About":
+                    var f = new form_message();
+                    f.ShowDialog();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            Close();
+        }
+    }
+}
